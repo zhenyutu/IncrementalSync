19fc61bbbf0f2d8930609e85dc00a05dad6f3811
zhenyutu <1572629866@qq.com>
2017-06-06 18:47:45 +0800

    完成单page解析

--- src/main/java/com/alibaba/middleware/race/sync/Main.java
+++ src/main/java/com/alibaba/middleware/race/sync/Main.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
  */
 public class Main {
     private static final int PAGE_SIZE = 4*1024*1024;
+    private static int position;
 
     public static void main(String[] args) throws IOException {
         String file = "/home/tuzhenyu/tmp/canal_data/test.txt";
@@ -27,23 +28,44 @@ public class Main {
             byte[] bytes = new byte[buffer.remaining()];
             buffer.get(bytes);
             System.out.println(Arrays.toString(bytes));
+            System.out.println(bytes.length);
 
 //            System.out.println(findFirstByte(bytes,37,(byte)124));
 
             int start = 0, end;
             start = start + 37;
-            end = findFirstByte(bytes,start,(byte)124);
+            end = findFirstByte(bytes,start,(byte)124,2);
             String schema = getResultStr(bytes,start,end);
             System.out.println(schema);
+//            start = end;
+//            end = findFirstByte(bytes,start,(byte)124,1);
+//            String table = getResultStr(bytes,start,end);
+//            System.out.println(table);
             start = end;
-            end = findFirstByte(bytes,start,(byte)124);
-            String table = getResultStr(bytes,start,end);
-            System.out.println(table);
-            start = end;
-            end = findFirstByte(bytes,start,(byte)124);
+            end = findFirstByte(bytes,start,(byte)124,1);
             String operate = getResultStr(bytes,start,end);
             System.out.println(operate);
-            System.out.println(findFirstByte(bytes,end,(byte)10));
+            start = end;
+            position = end;
+            end = findFirstByte(bytes,end,(byte)10,1);
+            for (int n = 3;;n=n+3){
+                if (position+1<end){
+                    System.out.print(findSingleStr(bytes,position,(byte)124,3));
+                    System.out.print(" ");
+                }else {
+                    break;
+                }
+            }
+//            System.out.print(findSingleStr(bytes,end,(byte)124,3));
+//            System.out.print(" ");
+//            System.out.print(findSingleStr(bytes,end,(byte)124,6));
+//            System.out.print(" ");
+//            System.out.print(findSingleStr(bytes,end,(byte)124,9));
+//            System.out.print(" ");
+//            System.out.println(findSingleStr(bytes,end,(byte)124,12));
+
+            System.out.println(position);
+//            System.out.println(findFirstByte(bytes,end,(byte)10,1));
         }
 
         channel.close();
@@ -55,14 +77,36 @@ public class Main {
         return new String(schemaBytes);
     }
 
-    private static int findFirstByte(byte[] logs,int start,byte value){
+    private static int findFirstByte(byte[] logs,int start,byte value,int num){
         int index = start;
         for (int i=start+1;i<logs.length;i++){
             if (logs[i] == value){
-                index = i;
-                break;
+                num--;
+                if (num<=0){
+                    index = i;
+                    break;
+                }
             }
         }
         return index;
+    }
+
+    private static String findSingleStr(byte[] logs,int start,byte value,int num){
+        int index = start;
+        int end = start;
+        for (int i=start+1;i<logs.length;i++){
+            if (logs[i] == value){
+                num--;
+                if (num<=0){
+                    end = i;
+                    break;
+                }
+                index = i;
+            }
+        }
+        position = end;
+        byte[] schemaBytes = new byte[end-index-1];
+        System.arraycopy(logs,index+1,schemaBytes,0,end-index-1);
+        return new String(schemaBytes);
     }
 }
--- src/main/java/com/alibaba/middleware/race/sync/ServerDemoInHandler.java
+++ src/main/java/com/alibaba/middleware/race/sync/ServerDemoInHandler.java
@@ -1,6 +1,5 @@
 package com.alibaba.middleware.race.sync;
 
-import io.netty.handler.codec.string.LineSeparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +13,11 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 处理client端的请求 Created by wanshao on 2017/5/25.
@@ -26,8 +26,15 @@ public class ServerDemoInHandler extends ChannelInboundHandlerAdapter {
 
     private static Logger logger = LoggerFactory.getLogger(ServerDemoInHandler.class);
     private static final int PAGE_SIZE = 4*1024;
+    private static final int REMAINING_SIZE = 20;
     private static final byte SPLITE_FLAG = (byte)124;
     private static final byte END_FLAG = (byte)10;
+    private static final byte SPACE_FLAG = (byte)32;
+
+    private int position = 0;
+
+    private Map<String,ByteBuffer> fileByteBuffer = new HashMap<>();
+    private Map<String,FileChannel> fileChannel = new HashMap<>();
 
     /**
      * 根据channel
@@ -108,61 +115,115 @@ public class ServerDemoInHandler extends ChannelInboundHandlerAdapter {
         channel.close();
     }
 
-    private void getLogFromBytes(byte[] logs){
+    private void getLogFromBytes(byte[] logs) throws IOException{
         int start = 0, end;
         while (true){
             start = start + 37;
-            end = findFirstByte(logs,start,SPLITE_FLAG);
-            String schema = getStrFromBytes(logs,start,end);
+            end = findFirstByte(logs,start,SPLITE_FLAG,2);
+            String schemaTable = getStrFromBytes(logs,start,end);
             start = end;
-            end = findFirstByte(logs,start,SPLITE_FLAG);
-            String table = getStrFromBytes(logs,start,end);
-            start = end;
-            end = findFirstByte(logs,start,SPLITE_FLAG);
+            end = findFirstByte(logs,start,SPLITE_FLAG,1);
             String operate = getStrFromBytes(logs,start,end);
             start = end;
-            end = findFirstByte(logs,end,END_FLAG);
+            position = end;
+            end = findFirstByte(logs,end,END_FLAG,1);
 
             switch (operate){
                 case "I":
-                    insertOperate(schema, table, operate, logs, start, end);
+                    insertOperate(schemaTable, logs, start, end);
                     break;
                 case "U":
-                    updateOperate(schema, table, operate, logs, start, end);
+                    updateOperate(schemaTable, logs, start, end);
                     break;
                 case "D":
-                    deleteOperate(schema, table, operate, logs, start, end);
+                    deleteOperate(schemaTable, logs, start, end);
                     break;
             }
-
-            start = end;
+            if (end>=logs.length-1)
+                break;
+            start = end+1;
         }
     }
 
     private String getStrFromBytes(byte[] logs,int start,int end){
         byte[] schemaBytes = new byte[end-start-1];
-        System.arraycopy(schemaBytes,start+1,schemaBytes,0,end-start-1);
+        System.arraycopy(logs,start+1,schemaBytes,0,end-start-1);
         return new String(schemaBytes);
     }
 
-    private int findFirstByte(byte[] logs,int start,byte value){
+    private int findFirstByte(byte[] logs,int start,byte value,int num){
         int index = start;
-        for (int i=start;i<logs.length;i++){
+        for (int i=start+1;i<logs.length;i++){
             if (logs[i] == value){
-                index = i;
-                break;
+                num--;
+                if (num<=0){
+                    index = i;
+                    break;
+                }
             }
         }
         return index;
     }
 
-    private void insertOperate(String schema,String table,String operate,byte[] logs,int start,int end){
+    private byte[] findSingleStr(byte[] logs,int start,byte value,int num){
+        int index = start;
+        int end = start;
+        for (int i=start+1;i<logs.length;i++){
+            if (logs[i] == value){
+                num--;
+                if (num<=0){
+                    end = i;
+                    break;
+                }
+                index = i;
+            }
+        }
+        position = end;
+        byte[] schemaBytes = new byte[end-index-1];
+        System.arraycopy(logs,index+1,schemaBytes,0,end-index-1);
+        return schemaBytes;
+    }
+
+    private void insertOperate(String schemaTable,byte[] logs,int start,int end)throws IOException{
+        ByteBuffer buffer = fileByteBuffer.get(schemaTable);
+        if (buffer==null){
+            buffer = ByteBuffer.allocate(PAGE_SIZE);
+            fileByteBuffer.put(schemaTable,buffer);
+        }
+
+        buffer.put(findSingleStr(logs,start,SPLITE_FLAG,3));
+        for (int n = 3;;n=n+3){
+            if (position+1<end){
+                buffer.put(SPACE_FLAG);
+                buffer.put(findSingleStr(logs,position,SPLITE_FLAG,3));
+            }else {
+                break;
+            }
+        }
+        buffer.put(END_FLAG);
+//        if (buffer.remaining()<REMAINING_SIZE)
+//        {
+            FileChannel channel = fileChannel.get(schemaTable);
+            if (channel==null){
+                String fileName = Constants.TESTER_HOME+"/"+schemaTable+".txt";
+                channel = new RandomAccessFile(fileName, "rw").getChannel();
+                fileChannel.put(schemaTable,channel);
+            }
+            buffer.flip();
+            channel.write(buffer);
+            buffer.clear();
+//        }
+    }
+    private void updateOperate(String schemaTable,byte[] logs,int start,int end){
 
     }
-    private void updateOperate(String schema,String table,String operate,byte[] logs,int start,int end){
+    private void deleteOperate(String schemaTable,byte[] logs,int start,int end){
 
     }
-    private void deleteOperate(String schema,String table,String operate,byte[] logs,int start,int end){
 
+    public static void main(String[] args) throws IOException{
+        ServerDemoInHandler handler = new ServerDemoInHandler();
+        String file = "/home/tuzhenyu/tmp/canal_data/canal.txt";
+        handler.pullBytesFormFile(file);
     }
 }
