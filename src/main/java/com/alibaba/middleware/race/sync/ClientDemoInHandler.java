2317260bc35974b2c0d8445a1e53017a91e326cd
zhenyutu <1572629866@qq.com>
2017-06-12 09:01:35 +0800

    修改文件写入方法

--- src/main/java/com/alibaba/middleware/race/sync/ClientDemoInHandler.java
+++ src/main/java/com/alibaba/middleware/race/sync/ClientDemoInHandler.java
@@ -7,12 +7,11 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.zip.Inflater;
 
@@ -55,14 +54,17 @@ public class ClientDemoInHandler extends ChannelInboundHandlerAdapter {
     private void writeBytes(byte[] bytes) throws IOException{
         logger.info("get into the writeBytes");
         String fileName = Constants.RESULT_HOME+"/Result.rs";
-        FileChannel channel = new RandomAccessFile(fileName, "rw").getChannel();
-        channel.write(ByteBuffer.wrap(bytes));
-        channel.force(true);
-        channel.close();
+
+        FileOutputStream fos = new FileOutputStream(fileName);
+        fos.write(bytes);
+        fos.flush();
+        fos.close();
         logger.info("finish write data to Result.rs");
         File file = new File(fileName);
         logger.info(file.getPath());
         logger.info("the file exist:"+file.exists());
+        String md5 = getFileMD5(file);
+        logger.info("the file md5 :"+md5);
     }
 
     public static byte[] uncompress(byte[] inputByte) throws IOException {
@@ -86,5 +88,28 @@ public class ClientDemoInHandler extends ChannelInboundHandlerAdapter {
             bos.close();
         }
         return bos.toByteArray();
+    }
+
+    public static String getFileMD5(File file) {
+        if (!file.isFile()){
+            return null;
+        }
+        MessageDigest digest = null;
+        FileInputStream in=null;
+        byte buffer[] = new byte[1024];
+        int len;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer, 0, 1024)) != -1) {
+                digest.update(buffer, 0, len);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger bigInt = new BigInteger(1, digest.digest());
+        return bigInt.toString(16);
     }
 }
